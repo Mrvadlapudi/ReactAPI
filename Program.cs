d5a2bf6c2273cc213875fb1696d589c3ca88b242
Mrvadlapudi <vadlapudikoti@example.com>
2025-03-02 16:57:10 +0530

    Fix: Use dynamic port from environment variable

--- Program.cs
+++ Program.cs
@@ -28,5 +28,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
+app.Run($"http://0.0.0.0:{port}");
+
 
 app.Run();
